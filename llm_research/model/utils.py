b318f2fcb5dfbca08a9a1e6a5b9079c7ce5bde18
githubjacky <jackyyeh@Jackys-MacBook-Air.local>
2024-05-29 22:04:19 +0800

    update read_jsonl utils

--- llm_research/model/utils.py
+++ llm_research/model/utils.py
@@ -27,16 +27,22 @@ def env_setup(tag: str = "OPENAI", env_file: str = '.env') -> None:
                 f.write(f"{key_name}={key}\n")
 
 
-def read_jsonl(
-    path: str | Path,
-    n: int = -1,
-    return_str: bool = False
-) -> List[Dict] | List[str]:
-    return (
-        [orjson.loads(i) for i in Path(path).read_text().split("\n")[:n]]
-        if not return_str
-        else
-        [i for i in Path(path).read_text().split("\n")[:n]]
-    )
+def read_jsonl(path: str | Path,
+               n: int = -1,
+               return_str: bool = False) -> List[Dict] | List[str]:
 
+    res = Path(path).read_text().split('\n')
+    if n == len(res):
+        n = -1
 
+    if not return_str and n == -1 and res[-1] != '':
+        return [orjson.loads(i) for i in res]
+    if return_str and n == -1 and res[-1] != '':
+        return res
+    else:
+         return (
+            [orjson.loads(i) for i in res[:n]]
+            if not return_str
+            else
+            res[:n]
+        )
